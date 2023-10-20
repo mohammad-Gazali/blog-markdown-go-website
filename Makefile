522749c8eaa4a4ad1796d3b5b1f18c3190b2d0c3
MohamadAlgazli <brawjym@gmail.com>
2023-10-20 17:51:47 +0300

    adding serving static files

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
+# you can use here "npm run" or yarn instead of pnpm
 build:
 	go build -o ./bin/main main.go
+	pnpm build
 
 run: build
 	./bin/main
--- router/router.go
+++ router/router.go
@@ -7,10 +7,17 @@ import (
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
+	serveStaticFiles(mux, "static")
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		MustGetTemplate(w, "index.html", struct{}{})
+		MustGetTemplate(w, "index.html", nil)
 	})
 
 	return mux
+}
+
+func serveStaticFiles(mux *http.ServeMux, folderName string) {
+	fs := http.FileServer(http.Dir(folderName))
+	mux.Handle("/" + folderName + "/", http.StripPrefix("/" + folderName, fs))
 }
